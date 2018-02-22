0e913a627120e0d7c426191096d144d3e727b759
Gowtham Genupur <gowtham.gourov@gmail.com>
2018-02-22 16:06:24 +0530

    exception handled for weak password

--- app/src/main/java/com/example/gowthamg/firebase1/Sign_Up_Activity.java
+++ app/src/main/java/com/example/gowthamg/firebase1/Sign_Up_Activity.java
@@ -1,14 +1,21 @@
 package com.example.gowthamg.firebase1;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class Sign_Up_Activity extends AppCompatActivity implements View.OnClickListener {
 
@@ -40,7 +47,7 @@ public class Sign_Up_Activity extends AppCompatActivity implements View.OnClickL
     @Override
     protected void onStart() {
         super.onStart();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
+      //  FirebaseUser currentUser = mAuth.getCurrentUser();
 
     }
 
@@ -55,13 +62,49 @@ public class Sign_Up_Activity extends AppCompatActivity implements View.OnClickL
         Entered_Username = username.getText().toString();
         Entered_Password = password.getText().toString();
 
-        Toast.makeText(this, "Email: "+Entered_Username+" Password: "+Entered_Password, Toast.LENGTH_SHORT).show();
+       // Toast.makeText(this, "Email: "+Entered_Username+" Password: "+Entered_Password, Toast.LENGTH_SHORT).show();
 
         if(!Entered_Username.equals(""))
         {
             if(!Entered_Password.equals(""))
             {
-                Toast.makeText(this, "All ok", Toast.LENGTH_SHORT).show();
+               mAuth.createUserWithEmailAndPassword(Entered_Username, Entered_Password)
+                       .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                           @Override
+                           public void onComplete(@NonNull Task<AuthResult> task)
+                           {
+                               if(task.isSuccessful())
+                               {
+                                   Toast.makeText(Sign_Up_Activity.this, "user created", Toast.LENGTH_SHORT).show();
+                               }
+                               else
+                               {
+                                   if(!task.isSuccessful()) {
+                                       try {
+                                           throw task.getException();
+                                       }
+                                       catch(FirebaseAuthWeakPasswordException e)
+                                       {
+                                           Toast.makeText(Sign_Up_Activity.this, "Weak Password", Toast.LENGTH_SHORT).show();
+                                       }
+                                       catch(FirebaseAuthInvalidCredentialsException e) {
+
+                                       }
+                                       catch(FirebaseAuthUserCollisionException e) {
+
+                                       }
+                                       catch(Exception e) {
+                                           Log.e("General Exception", e.getMessage());
+                                       }
+                                   }
+                                   Log.d("sign up error", "createUserWithEmail:failure", task.getException());
+                                   //Toast.makeText(Sign_Up_Activity.this, "Error: unable to create user", Toast.LENGTH_SHORT).show();
+                               }
+
+
+
+                           }
+                       });
             }
             else
                 Toast.makeText(this, "Please enter the password!", Toast.LENGTH_SHORT).show();
